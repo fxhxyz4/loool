13c356d031fb13e11b17dcb24718a63ac4249a7a
fxhxyz4 <fxhxyz@proton.me>
2023-01-09 11:49:00 +0200

    -

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 const btn = document.getElementById('btn')
 const div = document.getElementById('div')
 const heh = document.getElementById('heh')
+let huh = document.getElementById('huh2')
+let lul = document.getElementById('huh')
 alert(`epilepsy warning`)
 
 btn.addEventListener('click', function () {
@@ -12,4 +14,12 @@ heh.addEventListener('click', function () {
 	document.getElementsByClassName('main')[0].style.height = '100%'
 	document.getElementById('lol').textContent = "lol i'm scam you 😂🤣😂🤣"
 	btn.classList.add(`is-hidden`)
+})
+
+huh.disabled = false
+lul.disabled = false
+
+huh.addEventListener('click', function () {
+	lul.checked = true
+	huh.disabled = true
 })
